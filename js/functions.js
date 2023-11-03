b6bd84b7d404953318242f0747b87ceecab11f12
juanjoseguerrer <jjguerrreroa@ufpso.edu.co>
2023-11-03 14:08:09 -0500

    primeros eventos de javascript

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+
 //alert("FUNCTION ACTIVO");
 //############# Tipo de Variables
 // const para valores constantes
@@ -5,7 +6,7 @@ const pi = 3.1415;
 const pul = 2.54;
 //var para variables globales
 var days = 10;
-var name = "Wilder duarte";
+var name = "Juan Jose";
 //let para variables de bloque o locales
 let count = 1;
 
@@ -16,8 +17,8 @@ let count = 1;
 console.log(name);
 //body - pantalla
 document.write(name + "Wilder duarte");
-document.getElementById("box_one").innerHTML = "<h1>wilder duarte </h1>";
-document.getElementById("box_two").innerText = "<h1>wilder duarte </h1>";
+document.getElementById("box_one").innerHTML = "<h1>Juan Jose </h1>";
+document.getElementById("box_two").innerText = "<h1>Juan Jose </h1>";
 
 //librerias
 //Swal.fire({
@@ -30,7 +31,7 @@ document.getElementById("box_two").innerText = "<h1>wilder duarte </h1>";
 //    showCloseButton: true
 // })
 
-
+/*
 Swal.fire({
     icon:"error",
     showConfirmButton: false,
@@ -43,7 +44,7 @@ Swal.fire({
     });
 
 
-
+*/
 
 
 // ########### tipos de datos
@@ -79,6 +80,86 @@ var array_mul = [
     {name:"Antonella",last_name:"lopez", age:"41"}
 ]
 
+//Operadores logicos y estructuras condicionales
+//And && con if - Ampersand
+
+var bool = false;
+var numeric = "10";
+if(!bool && numeric === 10){ //bool == true
+    console.log("entra if");
 
 
+}
 
+else{
+    console.log("entra else ")
+}
+
+if(!bool || numeric == 7){
+    console.log("entra if");
+    numeric++;
+}
+
+else{
+    console.log("entra else");
+    numeric--;
+}
+
+
+//for
+
+for(let j = 0; j<array_text.length; j++){
+    console.log(array_text[j] + " "+ (j+1));
+}
+
+let f = 0;
+while(f<array_text.length){
+    console.log(array_text[f] + (f+1));
+    f++;
+
+}
+
+//do while
+let z = 0;
+do{
+    console.log(array_text[z] + (z+1));
+    z++;
+}while(z<array_text.length);
+
+/*
+function load_page(){
+    alert("la pagina se ha cargado completamente")
+}
+*/
+function change_color(){
+    document.body.style.backgroundColor = "#000000";
+    document.body.style.color = "#00FF00";
+}
+
+const btn_limpiar = document.querySelector("#clear_color");
+clear_color.addEventListener("click", () =>{
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+});
+
+//registro formulario
+const form_register = document.getElementById("form_register");
+const nombres = document.getElementById("nombres");
+const apellidos = document.getElementById("apellidos");
+const information = document.getElementById("information");
+
+form_register.addEventListener("submit", name_event => {
+    name_event.preventDefault();
+    let info = "";
+    if(nombres.value.length <= 2 || apellidos.value.length <= 2 ){
+        alert("nombres o apellidos invalidos");
+        info+= "nombres o apellidos invalidos";
+        information.style.color = "red"
+    }
+    else{
+        alert(nombres.value + "" + apellidos.value);
+
+    }
+    information.innerText = info;
+})
